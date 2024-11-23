78fac34e3677402566b8c5d0433dbed874a998ef
GDMontsho <montshogoitseone@gmail.com>
2024-11-23 10:32:27 +0100

    img files resolve

--- src/script.js
+++ src/script.js
@@ -25,40 +25,60 @@ function displayPokemonChar(pokemon) {
   const pokemonCard = document.createElement("div");
   pokemonCard.classList.add("card");
 
+  const cardInner = document.createElement("div");
+  cardInner.classList.add("card-inner");
+
+  const cardFront = document.createElement("div");
+  cardFront.classList.add("card-back");
+
   const pokemonName = document.createElement("p");
   pokemonName.classList.add("pokemonName");
   pokemonName.textContent =
     pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
-  pokemonCard.appendChild(pokemonName);
+  cardFront.appendChild(pokemonName);
 
   const pokemonImg = document.createElement("img");
   pokemonImg.src = pokemon.sprites.front_default;
   pokemonImg.alt = pokemon.name;
-  pokemonCard.appendChild(pokemonImg);
+  cardFront.appendChild(pokemonImg);
 
   const abilities = document.createElement("div");
   abilities.classList.add("abilities");
-  abilities.innerHTML = `<p> <b> PokeMon Abilities:</b></p>`;
+  abilities.innerHTML = `<p><b>Pokémon Abilities:</b></p>`;
   pokemon.abilities.forEach((ability) => {
     const abilityEl = document.createElement("p");
     abilityEl.textContent = ability.ability.name;
     abilities.appendChild(abilityEl);
   });
-
-  pokemonCard.appendChild(abilities);
+  cardFront.appendChild(abilities);
 
   const stats = document.createElement("div");
   stats.classList.add("stats");
   stats.innerHTML = `<p><b>Pokémon Stats:</b></p>`;
-
   pokemon.stats.forEach((stat) => {
-    console.log(stat);
     const statEl = document.createElement("p");
-    statEl.textContent = `${stat.stat.name}: ${stat.base_stat}`;
+
+    const statNameSpan = document.createElement("span");
+    statNameSpan.classList.add("stat-name");
+    statNameSpan.textContent = stat.stat.name;
+
+    const statValueSpan = document.createElement("span");
+    statValueSpan.classList.add("stat-value");
+    statValueSpan.textContent = stat.base_stat;
+
+    statEl.appendChild(statNameSpan);
+    statEl.appendChild(statValueSpan);
+
     stats.appendChild(statEl);
   });
+  cardFront.appendChild(stats);
 
-  pokemonCard.appendChild(stats);
+  const cardBack = document.createElement("div");
+  cardBack.classList.add("card-front");
 
+  cardInner.appendChild(cardFront);
+  cardInner.appendChild(cardBack);
+
+  pokemonCard.appendChild(cardInner);
   container.appendChild(pokemonCard);
 }
